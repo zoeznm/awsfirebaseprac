a760218eed351db8bc07fa70db7dc784adbff5a4
zoeznm <bomikim046@gmail.com>
2024-08-20 23:50:13 +0900

    firebase 배포 성공

---
+++ my-webpack-project/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production', // 또는 'development'
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
